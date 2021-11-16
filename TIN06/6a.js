a6684f5a7a81490f291b6ffc9c0b4fc23a04f8f5
Valerii <s20970@pjwstk.edu.pl>
2021-11-16 10:56:56 +0100

    Update 6a.js

--- TIN06/6a.js
+++ TIN06/6a.js
@@ -1,14 +1,11 @@
-C_input = document.getElementById("C_input");
-F_input = document.getElementById("F_input");
+let CF;
 
 function CtoF () {
-    C_val = C_input.value;
-    F_val = C_val * (9 / 5) + 32;
-    F_input.value = F_val;
+  let CF = (document.getElementById("C_input").value * 9) / 5 + 32;
+  document.getElementById("F_input").value = Math.round(CF);
 };
 
 function FtoC () {
-    F_val = F_input.value;
-  C_val = ((F_val - 32) * 5) / 9;
-  C_input.value = C_val;
+  let CF = ((document.getElementById("F_input").value - 32) * 5) / 9;
+  document.getElementById("C_input").value = Math.round(CF);
 };
